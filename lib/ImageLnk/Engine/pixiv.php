2707cea707bb9d149c84b03b9a94ca8aefc462d6
Takayama Fumihiko <tekezo@pqrs.org>
2014-05-19 13:47:47 +0900

    update pixiv

--- lib/ImageLnk/Engine/pixiv.php
+++ lib/ImageLnk/Engine/pixiv.php
@@ -17,14 +17,21 @@ class ImageLnk_Engine_pixiv {
 
     // --------------------
     // If mode=medium, fetch large image page
-    if (preg_match('/\?mode=medium&/', $url)) {
+    $url_info = parse_url($url);
+    if (! isset($url_info['query'])) return FALSE;
+
+    $query = array();
+    parse_str($url_info['query'], $query);
+    if (! isset($query['mode'])) return FALSE;
+
+    if ($query['mode'] == 'medium') {
       if (preg_match('/<div class="works_display">(.*?)<\/div>/s', $html, $matches)) {
         if (preg_match('/ href="(member_illust.php\?mode=big&.*?)"/', $matches[1], $m)) {
           $newurl = 'http://www.pixiv.net/' . html_entity_decode($m[1], ENT_QUOTES, 'UTF-8');
           return self::handle($newurl);
         }
         if (preg_match('/ href="(member_illust.php\?mode=manga&.*?)"/', $matches[1], $m)) {
-          $newurl = preg_replace('/mode=manga&/', 'mode=manga_big&', $m[1]);
+          $newurl = preg_replace('/mode=manga/', 'mode=manga_big', $m[1]);
           $newurl = 'http://www.pixiv.net/' . html_entity_decode($newurl, ENT_QUOTES, 'UTF-8') . '&page=0';
           return self::handle($newurl);
         }
@@ -35,7 +42,7 @@ class ImageLnk_Engine_pixiv {
     $response = new ImageLnk_Response();
     $response->setReferer($url);
 
-    if (preg_match('/member_illust\.php\?mode=(manga_big|big)/', $url)) {
+    if ($query['mode'] == 'big' || $query['mode'] == 'manga_big') {
       $response->setTitle(ImageLnk_Helper::getTitle($html));
       foreach (ImageLnk_Helper::scanSingleTag('img', $html) as $img) {
         if (preg_match('/src="(.+?)"/', $img, $m)) {
@@ -43,7 +50,7 @@ class ImageLnk_Engine_pixiv {
         }
       }
 
-    } elseif (preg_match('/member_illust\.php\?mode=manga/', $url)) {
+    } elseif ($query['mode'] == 'manga') {
       $response->setTitle(ImageLnk_Helper::getTitle($html));
       if (preg_match_all("/pixiv.context.images\[\d\] = '(.+?)';/", $html, $m)) {
         foreach ($m[1] as $imgsrc) {
