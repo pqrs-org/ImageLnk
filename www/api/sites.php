8f15e7a5ebcb9239bee32ea3d5b803d99ddad122
Takayama Fumihiko <tekezo@pqrs.org>
2015-02-08 13:52:40 +0000

    apply phpcbf

--- www/api/sites.php
+++ www/api/sites.php
@@ -2,11 +2,13 @@
 
 require_once sprintf('%s/../../lib/ImageLnk.php', dirname(__FILE__));
 
-class ImageLnkAPI_sites {
-  public function control() {
-    header('Content-Type: application/json');
-    print json_encode(array('sites' => ImageLnk::getSites()));
-  }
+class ImageLnkAPI_sites
+{
+    public function control()
+    {
+        header('Content-Type: application/json');
+        print json_encode(array('sites' => ImageLnk::getSites()));
+    }
 }
 
 $imagelnk = new ImageLnkAPI_sites();
