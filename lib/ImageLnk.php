847e6a4efa3e81af5550fa5af3401c582f7d4ec1
Takayama Fumihiko <tekezo@pqrs.org>
2015-02-08 13:51:44 +0000

    apply phpcbf

--- lib/ImageLnk.php
+++ lib/ImageLnk.php
@@ -9,19 +9,20 @@ require_once 'SymfonyComponents/YAML/sfYaml.php';
 // ------------------------------------------------------------
 function ImageLnk_autoload($className)
 {
-  $replaces = array(
-    '_'  => DIRECTORY_SEPARATOR,
-    '::' => DIRECTORY_SEPARATOR,
-    '.'  => '',
+    $replaces = array(
+        '_'  => DIRECTORY_SEPARATOR,
+        '::' => DIRECTORY_SEPARATOR,
+        '.'  => '',
     );
-  $classPath = str_replace(array_keys($replaces), array_values($replaces), $className);
-  $fileName = join(DIRECTORY_SEPARATOR,
-                   array(dirname(__FILE__), $classPath . '.php'));
+    $classPath = str_replace(array_keys($replaces), array_values($replaces), $className);
+    $fileName = join(
+        DIRECTORY_SEPARATOR,
+        array(dirname(__FILE__), $classPath . '.php')
+    );
 
-  if (is_file($fileName))
-  {
-    require_once $fileName;
-  }
+    if (is_file($fileName)) {
+        include_once $fileName;
+    }
 }
 spl_autoload_register('ImageLnk_autoload');
 
@@ -30,41 +31,45 @@ ImageLnk_Config::static_initialize();
 
 // ------------------------------------------------------------
 foreach (glob(sprintf('%s/ImageLnk/Engine/*.php', dirname(__FILE__))) as $file) {
-  require_once $file;
+    include_once $file;
 }
 
 // ------------------------------------------------------------
-class ImageLnk {
-  public static function getImageInfo($url) {
-    $url = ImageLnk_URL::getRedirectedURL($url);
-    foreach (ImageLnk_Engine::getEngines() as $classname) {
-      try {
-        $response = $classname::handle($url);
-        if ($response !== FALSE) {
-          return $response;
+class ImageLnk
+{
+    public static function getImageInfo($url)
+    {
+        $url = ImageLnk_URL::getRedirectedURL($url);
+        foreach (ImageLnk_Engine::getEngines() as $classname) {
+            try {
+                $response = $classname::handle($url);
+                if ($response !== false) {
+                    return $response;
+                }
+            } catch (Exception $e) {
+                error_log('getImageInfo got Exception: ' . $e->getMessage());
+                error_log($e->getTraceAsString());
+            }
         }
-      } catch (Exception $e) {
-        error_log('getImageInfo got Exception: ' . $e->getMessage());
-        error_log($e->getTraceAsString());
-      }
+        return false;
     }
-    return FALSE;
-  }
 
-  public static function getSites() {
-    $sites_generic  = array();
-    $sites_domestic = array();
-    foreach (ImageLnk_Engine::getEngines() as $classname) {
-      if (! $classname::sitename) continue;
+    public static function getSites()
+    {
+        $sites_generic  = array();
+        $sites_domestic = array();
+        foreach (ImageLnk_Engine::getEngines() as $classname) {
+            if (! $classname::sitename) { continue;
+            }
 
-      if ($classname::language) {
-        $sites_domestic[] = $classname::sitename;
-      } else {
-        $sites_generic[] = $classname::sitename;
-      }
+            if ($classname::language) {
+                $sites_domestic[] = $classname::sitename;
+            } else {
+                $sites_generic[] = $classname::sitename;
+            }
+        }
+        sort($sites_generic);
+        sort($sites_domestic);
+        return array_merge($sites_generic, $sites_domestic);
     }
-    sort($sites_generic);
-    sort($sites_domestic);
-    return array_merge($sites_generic, $sites_domestic);
-  }
 }
