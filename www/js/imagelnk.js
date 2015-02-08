25adc82e22058e25cc1788e0f5634336b2bab484
Takayama Fumihiko <tekezo@pqrs.org>
2015-02-08 13:54:01 +0000

    fix code style

--- www/js/imagelnk.js
+++ www/js/imagelnk.js
@@ -1,57 +1,57 @@
 // -*- Mode: js2; indent-tabs-mode: nil; -*-
 
-jQuery(
+$(
     function() {
-           jQuery.getJSON(
-               'api/sites', function(data) {
-                              var sites = [];
-                              var length = data['sites'].length;
-                    for (var i = 0; i < length; ++i) {
-                        jQuery(
-                            '<div/>', {
-                                html: data['sites'][i]
-                               }
-                        ).appendTo('#sites');
-                    }
+        $.getJSON(
+            'api/sites', function(data) {
+                var sites = [];
+                var length = data['sites'].length;
+                for (var i = 0; i < length; ++i) {
+                    $(
+                        '<div/>', {
+                            html: data['sites'][i]
+                        }
+                    ).appendTo('#sites');
                 }
-           );
-            jQuery('#showinfo').bind(
-                'click', function() {
-                                        jQuery('#showinfo_result_pageurl').text('Loading...');
-                                        jQuery('#showinfo_result_title').text('Loading...');
-                                        jQuery('#showinfo_result_referer').text('Loading...');
-                                        jQuery('#showinfo_result_backlink').text('Loading...');
-                                        jQuery('#showinfo_result_imageurls').text('Loading...');
+            }
+        );
+        $('#showinfo').bind(
+            'click', function() {
+                $('#showinfo_result_pageurl').text('Loading...');
+                $('#showinfo_result_title').text('Loading...');
+                $('#showinfo_result_referer').text('Loading...');
+                $('#showinfo_result_backlink').text('Loading...');
+                $('#showinfo_result_imageurls').text('Loading...');
 
-                    if (jQuery('#showinfo_result').css('display') != 'none') {
-                        jQuery('#showinfo_result').hide();
-                    }
-                                        jQuery('#showinfo_result').slideDown('slow');
-
-                                        jQuery.getJSON(
-                                            'api/get',
-                                            {
-                                                url: jQuery('#url').val()
-                                                        },
-                                            function(data) {
-                                                if (data.title     == undefined) { data.title     = 'N/A'; }
-                                                if (data.referer   == undefined) { data.referer   = 'N/A'; }
-                                                if (data.backlink  == undefined) { data.backlink  = 'N/A'; }
-                                                if (data.imageurls == undefined) { data.imageurls = ['N/A']; }
-
-                                                            jQuery('#showinfo_result_pageurl').text(data.pageurl);
-                                                            jQuery('#showinfo_result_title').text(data.title);
-                                                            jQuery('#showinfo_result_referer').text(data.referer);
-                                                            jQuery('#showinfo_result_backlink').text(data.backlink);
-                                                            var imageurls = '';
-                                                            var length = data.imageurls.length;
-                                                for (var i = 0; i < length; ++i) {
-                                                    imageurls += data.imageurls[i] + "\n";
-                                                }
-                                                            jQuery('#showinfo_result_imageurls').text(imageurls);
-                                            }
-                                        );
+                if ($('#showinfo_result').css('display') != 'none') {
+                    $('#showinfo_result').hide();
                 }
-            );
+                $('#showinfo_result').slideDown('slow');
+
+                $.getJSON(
+                    'api/get',
+                    {
+                        url: $('#url').val()
+                    },
+                    function(data) {
+                        if (data.title     == undefined) { data.title     = 'N/A'; }
+                        if (data.referer   == undefined) { data.referer   = 'N/A'; }
+                        if (data.backlink  == undefined) { data.backlink  = 'N/A'; }
+                        if (data.imageurls == undefined) { data.imageurls = ['N/A']; }
+
+                        $('#showinfo_result_pageurl').text(data.pageurl);
+                        $('#showinfo_result_title').text(data.title);
+                        $('#showinfo_result_referer').text(data.referer);
+                        $('#showinfo_result_backlink').text(data.backlink);
+                        var imageurls = '';
+                        var length = data.imageurls.length;
+                        for (var i = 0; i < length; ++i) {
+                            imageurls += data.imageurls[i] + "\n";
+                        }
+                        $('#showinfo_result_imageurls').text(imageurls);
+                    }
+                );
+            }
+        );
     }
 );
