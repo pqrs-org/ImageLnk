0b70a30ff348436289f07a21eb507ba18a6214c8
Takayama Fumihiko <tekezo@pqrs.org>
2015-08-16 10:58:10 +0900

    update twitpic

--- lib/ImageLnk/Engine/twitpic.php
+++ lib/ImageLnk/Engine/twitpic.php
@@ -1,5 +1,7 @@
 <?php //-*- Mode: php; indent-tabs-mode: nil; -*-
 
+use Sunra\PhpSimple\HtmlDomParser;
+
 class ImageLnk_Engine_twitpic
 {
     const language = null;
@@ -14,24 +16,19 @@ class ImageLnk_Engine_twitpic
         $id = $matches[1];
 
         // ----------------------------------------
-        if (! preg_match('/\/full$/', $url)) {
-            $url = "http://twitpic.com/$id/full";
-        }
-
-        // ----------------------------------------
         $data = ImageLnk_Cache::get($url);
         $html = $data['data'];
+
+        $dom = HtmlDomParser::str_get_html($html);
 
         $response = new ImageLnk_Response();
         $response->setReferer($url);
 
         $response->setTitle(ImageLnk_Helper::getTitle($html));
-        if (preg_match('%<div id="media-full">(.+?)</div>%s', $html, $matches)) {
-            foreach (ImageLnk_Helper::scanSingleTag('img', $matches[1]) as $img) {
-                if (preg_match('/ src="(.+?)"/', $img, $m)) {
-                    $response->addImageURL($m[1]);
-                }
-            }
+
+        $img = $dom->find('#media > img', 0);
+        if ($img) {
+            $response->addImageURL($img->src);
         }
 
         return $response;
