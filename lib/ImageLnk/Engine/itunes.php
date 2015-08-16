d5e721016f67bb612850557aa4b271a6a163fa66
Takayama Fumihiko <tekezo@pqrs.org>
2015-08-16 23:05:09 +0900

    clean up

--- lib/ImageLnk/Engine/itunes.php
+++ lib/ImageLnk/Engine/itunes.php
@@ -1,7 +1,5 @@
 <?php //-*- Mode: php; indent-tabs-mode: nil; -*-
 
-use Sunra\PhpSimple\HtmlDomParser;
-
 class ImageLnk_Engine_itunes
 {
     const language = null;
@@ -17,12 +15,10 @@ class ImageLnk_Engine_itunes
         $data = ImageLnk_Cache::get($url);
         $html = $data['data'];
 
-        $dom = HtmlDomParser::str_get_html($html);
-
         $response = new ImageLnk_Response();
         $response->setReferer($url);
-        $response->setTitle($dom->find('meta[property=og:title]', 0)->content);
-        $response->addImageURL($dom->find('meta[property=og:image]', 0)->content);
+
+        ImageLnk_Helper::setResponseFromOpenGraph($response, $html);
 
         return $response;
     }
