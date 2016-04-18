fb99bb49a23bb512b579238c069ab659f2f35964
Takayama Fumihiko <tekezo@pqrs.org>
2016-04-18 21:24:32 +0900

    update instagram

---
+++ lib/ImageLnk/Engine/instagram.php
@@ -0,0 +1,43 @@
+<?php //-*- Mode: php; indent-tabs-mode: nil; -*-
+
+use Sunra\PhpSimple\HtmlDomParser;
+
+class ImageLnk_Engine_instagram
+{
+    const language = null;
+    const sitename = 'https://www.instagram.com/';
+
+    public static function handle($url)
+    {
+        if (! preg_match('%^https://www\.instagram\.com/p/([^/]+)%', $url, $matches)) {
+            return false;
+        }
+
+        $shortcode = $matches[1];
+
+        // ----------------------------------------
+        $data = ImageLnk_Cache::get($url);
+        $html = $data['data'];
+
+        $dom = HtmlDomParser::str_get_html($html);
+
+        $response = new ImageLnk_Response();
+        $response->setReferer($url);
+
+        foreach ($dom->find('script') as $script) {
+            if (preg_match('/^window._sharedData = (.+);/', $script->innertext, $matches)) {
+                $sharedData = json_decode($matches[1]);
+
+                $caption = $sharedData->entry_data->PostPage[0]->media->caption;
+                $username = $sharedData->entry_data->PostPage[0]->media->owner->username;
+                $src = $sharedData->entry_data->PostPage[0]->media->display_src;
+
+                $response->setTitle(sprintf('@%s on Instagram: “%s”', $username, $caption));
+                $response->addImageURL($src);
+            }
+        }
+
+        return $response;
+    }
+}
+ImageLnk_Engine::push('ImageLnk_Engine_instagram');
