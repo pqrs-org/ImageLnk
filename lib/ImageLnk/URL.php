717dab5749a0677514c4bab7db599470bcb1001f
Takayama Fumihiko <tekezo@pqrs.org>
2017-03-15 14:27:22 +0900

    support multiple Location headers

--- lib/ImageLnk/URL.php
+++ lib/ImageLnk/URL.php
@@ -14,6 +14,9 @@ class ImageLnk_URL
         $headers = get_headers($url, 1);
         if ($headers !== false && isset($headers['Location'])) {
             $location = $headers['Location'];
+            if (is_array($location)) {
+                $location = $location[0];
+            }
             if (preg_match('/^\//', $location)) {
                 preg_match('#^(.+://.+?)/#', $url, $matches);
                 $location = $matches[1] . $location;
