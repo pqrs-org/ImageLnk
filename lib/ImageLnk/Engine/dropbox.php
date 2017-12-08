85411bd4d44ae08abbb5eb5907a545538ac15451
Takayama Fumihiko <tekezo@pqrs.org>
2017-12-08 09:09:55 +0900

    add dropbox

---
+++ lib/ImageLnk/Engine/dropbox.php
@@ -0,0 +1,40 @@
+<?php //-*- Mode: php; indent-tabs-mode: nil; -*-
+
+use Sunra\PhpSimple\HtmlDomParser;
+
+class ImageLnk_Engine_dropbox
+{
+    const language = null;
+    const sitename = 'https://www.dropbox.com/';
+
+    public static function handle($url)
+    {
+        if (! preg_match('%^https://www\.dropbox\.com%', $url)) {
+            return false;
+        }
+
+        // ----------------------------------------
+        $data = ImageLnk_Cache::get($url);
+        $html = $data['data'];
+
+        $dom = HtmlDomParser::str_get_html($html);
+
+        $response = new ImageLnk_Response();
+        $response->setReferer($url);
+
+        $response->setTitle(ImageLnk_Helper::getTitle($html));
+
+        $img = $dom->find('div.prerender-preview__wrapper img', 0);
+        if ($img) {
+            $srcset = $img->getAttribute('srcset');
+            $srcset = explode(',', $srcset);
+            $image = $srcset[count($srcset) - 1];
+            $image = trim($image);
+            $image = explode(' ', $image);
+            $response->addImageURL($image[0]);
+        }
+
+        return $response;
+    }
+}
+ImageLnk_Engine::push('ImageLnk_Engine_dropbox');
