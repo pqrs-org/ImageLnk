1568597a6c7ddbd5c8be7bd1cf66b3a630443fb8
Takayama Fumihiko <tekezo@pqrs.org>
2020-08-20 22:58:00 +0900

    Update USER_AGENT

--- lib/ImageLnk/Fetcher.php
+++ lib/ImageLnk/Fetcher.php
@@ -2,7 +2,7 @@
 
 class ImageLnk_Fetcher
 {
-    const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36";
+    const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36";
 
     protected static function getCookieCacheFilePath($site)
     {
